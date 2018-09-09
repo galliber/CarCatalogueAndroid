850e2cdcf0388b4130afaf5420b494c11a2277a7
Slavi <slavi_14@abv.bg>
2018-09-09 18:52:47 +0300

    async/http

--- app/src/main/java/com/example/wolverine/carcatalogueandroid/http/HttpRequester.java
+++ app/src/main/java/com/example/wolverine/carcatalogueandroid/http/HttpRequester.java
@@ -1,4 +1,10 @@
 package com.example.wolverine.carcatalogueandroid.http;
 
+import java.io.IOException;
+
 public interface HttpRequester {
+    String get(String url) throws IOException;
+    String post(String url, String body) throws IOException;
+    String delete(String url) throws IOException;
+    String put(String url, String body) throws IOException;
 }
