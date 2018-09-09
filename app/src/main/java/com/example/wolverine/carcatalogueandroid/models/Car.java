133f0609fbe4daa572ab581bb9396d8734e25899
Slavi <slavi_14@abv.bg>
2018-09-09 18:35:43 +0300

    models

--- app/src/main/java/com/example/wolverine/carcatalogueandroid/models/Car.java
+++ app/src/main/java/com/example/wolverine/carcatalogueandroid/models/Car.java
@@ -1,4 +1,44 @@
 package com.example.wolverine.carcatalogueandroid.models;
 
-public class Car {
+import java.io.Serializable;
+
+public class Car implements Serializable{
+    public int id;
+    public String make;
+    public String model;
+    public  int power;
+    public int cubicCapacity;
+    public String imgUrl;
+
+    public Car(int id, String make, String model, int power, int cubicCapacity,String imgUrl){
+        this.id=id;
+        this.make=make;
+        this.model=model;
+        this.power=power;
+        this.cubicCapacity=cubicCapacity;
+        this.imgUrl=imgUrl;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getPower() {
+        return power;
+    }
+
+    public int getCubicCapacity() {
+        return cubicCapacity;
+    }
+    public String getImgUrl(){
+        return imgUrl;
+    }
 }
